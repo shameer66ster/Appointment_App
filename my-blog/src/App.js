30a9ec9768b917738828a5ce7391bbcdee4864d4
Shameer Ebrahim <shameer66ster@gmail.com>
2024-04-18 16:23:55 +0530

    my first commit for this blog

---
+++ my-blog/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Awsome blog</h1>
+      <div id="page-body">Welcome to my blog!</div>
+    </div>
+  );
+}
+
+export default App;
