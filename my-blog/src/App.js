3f4179c3e5c6fbb68377e175d2edad97ef95a649
Shameer Ebrahim <shameer66ster@gmail.com>
2024-04-19 12:20:57 +0530

    Navbar and pages added

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticleListPage from "./pages/ArticleListPage";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awsome blog</h1>
-      <div id="page-body">Welcome to my blog!</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/article" element={<ArticleListPage />} />
+            <Route path="/articlepage/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
